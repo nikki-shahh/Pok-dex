a2510fe209b52825fa079ae44e5006ad1c396ee3
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2021-06-11 18:18:40 -0700

    Created Pokemon List Array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,38 @@
-alert ('Hello World');
-
-let favoriteFood= 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+  {
+    name: "Venusaur",
+    height: 2,
+    type: [ "Grass" , "Poison" ],
+    ability: [ "Chlorophyll" , "Overgrow" ]
+  },
+  {
+    name: "Raichu",
+    height: 0.8,
+    type: "Electric" ,
+    ability: [ "Static" , "Lightningrod" ]
+  },
+  {
+    name: "Nidoking",
+    height: 1.4,
+    type: [ "Ground" , "Poison" ],
+    ability: [ "Poison-point" , "Rivalry" , "Sheer-force"]
+  },
+  {
+    name: "Rhydon",
+    height: 1.9,
+    type: [ "Rock" , "Ground" ],
+    ability: [ "Rhydon" , "rock-head" , "Reckless" ]
+  },
+  {
+    name: "Torkoal",
+    height: 0.5,
+    type: "Fire",
+    ability: [ "White-smoke" , "Shell-armor" ]
+  },
+  {
+    name: "Poliwrath",
+    height: 1.3,
+    type: [ "Water" , "Fighting" ],
+    ability: [ "Damp" , "Water-absorb" , "Swift-swim"]
+  }
+];
