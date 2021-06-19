da6858c13ab29b741e574b66eb607add7c983906
nikki-shahh <84827772+nikki-shahh@users.noreply.github.com>
2021-06-19 12:46:26 -0700

    wraps pokemonList array in IIFE & adds an object with add function & prints list of pokemons w/ forEach

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,67 @@
-let pokemonList = [
-  {
-    name: "Venusaur",
-    height: 2,
-    type: [ "Grass" , "Poison" ],
-    ability: [ "Chlorophyll" , "Overgrow" ]
-  },
-  {
-    name: "Raichu",
-    height: 0.8,
-    type: "Electric" ,
-    ability: [ "Static" , "Lightningrod" ]
-  },
-  {
-    name: "Nidoking",
-    height: 1.4,
-    type: [ "Ground" , "Poison" ],
-    ability: [ "Poison-point" , "Rivalry" , "Sheer-force"]
-  },
-  {
-    name: "Rhydon",
-    height: 1.9,
-    type: [ "Rock" , "Ground" ],
-    ability: [ "Rhydon" , "rock-head" , "Reckless" ]
-  },
-  {
-    name: "Torkoal",
-    height: 0.5,
-    type: "Fire",
-    ability: [ "White-smoke" , "Shell-armor" ]
-  },
-  {
-    name: "Poliwrath",
-    height: 1.3,
-    type: [ "Water" , "Fighting" ],
-    ability: [ "Damp" , "Water-absorb" , "Swift-swim"]
-  }
-];
+let pokemonRepository = (function ()
+{
+    let pokemonList = [
+      {
+        name: " Venusaur",
+        height: 2,
+        type: [ " Grass" , " Poison" ],
+        ability: [ " Chlorophyll" , " Overgrow" ]
+      },
+      {
+        name: " Raichu",
+        height: 0.8,
+        type: " Electric" ,
+        ability: [ " Static" , " Lightningrod" ]
+      },
+      {
+        name: " Nidoking",
+        height: 1.4,
+        type: [ " Ground" , " Poison" ],
+        ability: [ " Poison-point" , " Rivalry" , " Sheer-force"]
+      },
+      {
+        name: " Rhydon",
+        height: 1.9,
+        type: [ " Rock" , " Ground" ],
+        ability: [ " Rhydon" , " rock-head" , " Reckless" ]
+      },
+      {
+        name: " Torkoal",
+        height: 0.5,
+        type: " Fire",
+        ability: [ " White-smoke" , " Shell-armor" ]
+      },
+      {
+        name: " Poliwrath",
+        height: 1.3,
+        type: [ " Water" , " Fighting" ],
+        ability: [ " Damp" , " Water-absorb" , " Swift-swim"]
+      }
+    ];
 
-pokemonList.forEach (function(pokemon)
+    function getAll (){
+      return pokemonList;
+    }
+    function add (pokemon){
+      return pokemonList.push(pokemon);
+    }
+    return {
+     getAll : getAll,
+     add : add
+   };
+
+})();
+
+pokemonRepository.add(
+  {
+    name: " Nidoqueen",
+    height: 1.3,
+    type: [" Ground"," Poison"],
+    ability: [" Poison Point", " Rivalry"]
+  }
+);
+
+pokemonRepository.getAll().forEach (function(pokemon)
 {
   //prints the details of each Pokemon to the DOM and add line breaks
  document.write ("<p> Name: " + pokemon.name + " - Height: " + pokemon.height + " - Type: " + pokemon.type + " - Ability: " + pokemon.ability);
